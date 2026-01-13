78cb4951a45716aebf02b2ffcfc7c89a8f29fdf9
BlasCasale <casale.blas@live.com>
2026-01-12 23:43:10 -0300

    termine seccion

--- 03-admin-todo/src/components/NewTodo.tsx
+++ 03-admin-todo/src/components/NewTodo.tsx
@@ -1,14 +1,39 @@
 'use client';
 
+import { createTodo, deleteTodos } from "@/todos/helpers/todos";
+import { useRouter } from "next/navigation";
+import { FormEvent, useState } from "react";
 import { IoTrashOutline } from "react-icons/io5";
 
 export const NewTodo = () => {
 
+  const [description, setDescription] = useState('')
+
+  const router = useRouter()
+
+  const onSubmit = async (e: FormEvent) => {
+    e.preventDefault()
+
+    if (description.trim()) {
+      createTodo(description.trim())
+      router.refresh()
+    }
+  }
+
+  const deleteCompleted = async () => {
+    await deleteTodos()
+    router.refresh()
+  }
+
   return (
-    <form className='flex w-full'>
+    <form className='flex w-full' onSubmit={onSubmit}>
       <input type="text"
+        value={description}
         className="w-6/12 -ml-10 pl-3 pr-3 py-2 rounded-lg border-2 border-gray-200 outline-none focus:border-sky-500 transition-all"
-        placeholder="¿Qué necesita ser hecho?" />
+        placeholder="¿Qué necesita ser hecho?"
+        id="description"
+        onChange={(e) => setDescription(e.target.value)}
+      />
 
       <button type='submit' className="flex items-center justify-center rounded ml-2 bg-sky-500 p-2 text-white hover:bg-sky-700 transition-all">
         Crear
@@ -17,7 +42,7 @@ export const NewTodo = () => {
       <span className='flex flex-1'></span>
 
       <button
-        //TODO: onClick={ () => deleteCompleted() }
+        onClick={deleteCompleted}
         type='button' className="flex items-center justify-center rounded ml-2 bg-red-400 p-2 text-white hover:bg-red-700 transition-all">
         <IoTrashOutline />
         Delete
--- 03-admin-todo/src/todos/helpers/todos.ts
+++ 03-admin-todo/src/todos/helpers/todos.ts
@@ -14,3 +14,28 @@ export const updateTodo = async (id: string, complete: boolean): Promise<Todo> =
 
   return todo
 }
+
+export const createTodo = async (description: string): Promise<Todo> => {
+
+  const body = JSON.stringify({ description })
+  const createdTodo = await fetch('/api/todos', {
+    method: 'POST',
+    body,
+    headers: {
+      'Content-Type': 'application/json'
+    }
+  }).then((res) => res.json())
+
+  return createdTodo;
+}
+
+export const deleteTodos = async (): Promise<Todo> => {
+  const deletedTodos = await fetch('/api/todos', {
+    method: 'DELETE',
+    headers: {
+      'Content-Type': 'application/json'
+    }
+  }).then((res) => res.json())
+
+  return deletedTodos
+}
